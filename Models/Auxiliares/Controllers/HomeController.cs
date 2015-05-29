42e7d809f8959a114a73784129136416ec037bfe
Gustavo Ovelar <gusove@gmail.com>
2015-05-29 11:01:53 -0300

    Uso de Cascading para prencher dropdownlist

--- Models/Auxiliares/Controllers/HomeController.cs
+++ Models/Auxiliares/Controllers/HomeController.cs
@@ -14,17 +14,21 @@ namespace WebServiceMVC.Models.Auxiliares.Controllers
         //variáveis de acesso ao repositório
         private PessoaBLL _bllPessoa;
         private ProdutoBLL _bllProduto;
-
+        private EstadoCidadeBLL _bllEstadoCidade;
         public HomeController()
         {
             _bllPessoa = new PessoaBLL();
             _bllProduto = new ProdutoBLL();
+            _bllEstadoCidade = new EstadoCidadeBLL();
         }
         public ActionResult Index()
         {
             ViewBag.Message = "Bem-vido ao exemplo prático ASP.NET MVC";
             // conecta a data base
             TestePraticoEntities db = new TestePraticoEntities();
+            // Recuperar estados e construi a SelectList.
+            ViewBag.Estado = new SelectList(db.tbEstado, "Id", "Estado");
+
             return View();
         }
 
@@ -40,6 +44,15 @@ namespace WebServiceMVC.Models.Auxiliares.Controllers
         {
             var cadastrado = _bllProduto.CadastrarProduto(collection);
             return Json(cadastrado ? new { Mensagem = "Cadastro realizado com sucesso!" } : new { Mensagem = "Houve um problema ao realizar o cadastro!" });
+        }
+
+        [HttpPost]
+        public ActionResult getEstadoByCidade(string selectedValue)
+        {
+            EstadoCidadeBLL EstadoCidade = new EstadoCidadeBLL();
+            List<string> estadoList = new List<string>();
+            estadoList = EstadoCidade.ObterCidade(Convert.ToInt32(selectedValue));
+            return Json(new { EstadoList = estadoList });
         }
     }
 }
---
+++ Models/Repositorio/EstadoCidadeBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceMVC.Models.Repositorio
+{
+    public class EstadoCidadeBLL
+    {
+        //Retorna a tabela tbEstado em uma Lista
+        public List<tbEstado> ObterEstado()
+        {
+            using (TestePraticoEntities _db = new TestePraticoEntities())
+            {
+                return _db.tbEstado.ToList();
+            }
+        }
+
+        //Retorna a tabela tbCidade em uma lista conforme ao estado
+        public List<string> ObterCidade(int idEstado)
+        {
+            using (TestePraticoEntities _db = new TestePraticoEntities())
+            {
+                return _db.tbCidade.Where(query => query.tbEstado_id == idEstado).Select(c => c.Cidade).ToList();
+            }
+        }
+    }
+}
